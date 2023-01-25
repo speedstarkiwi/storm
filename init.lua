2b78848eebbda9f4c0080b573f83e8c9536b8fe0
speedstarkiwi <121459772+speedstarkiwi@users.noreply.github.com>
2023-01-25 17:12:30 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -13,7 +13,7 @@ This wasnt obfuscated to decrease the impact of downloading, deobfuscating, etc.
 <3 speedstarskiwi, happy new years 2022!
 --]]
 
---- Injection watermark
+--[[
 local gmt = getrawmetatable(game)
         local old = gmt.__namecall
         local _game = game
@@ -56,14 +56,9 @@ end
 end
     return oldi(self, i)
 end
+--]]
 
-local TS = game:GetService('TweenService') local StormLoader = Instance.new('ScreenGui') local ImageLabel = Instance.new('ImageLabel') StormLoader.Name = 'StormLoader' StormLoader.Parent = game:GetService('CoreGui') StormLoader.ZIndexBehavior = Enum.ZIndexBehavior.Sibling StormLoader.Enabled = true ImageLabel.Parent = StormLoader ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255) ImageLabel.BackgroundTransparency = 1 ImageLabel.Position = UDim2.new(0.5, -100, 0.5, -100) ImageLabel.Size = UDim2.new(0, 200, 0, 200) ImageLabel.ZIndex = 1000877866 ImageLabel.Image = 'rbxassetid://12008234655' ImageLabel.ScaleType = Enum.ScaleType.Tile ImageLabel.ImageTransparency = 1 TS:Create(ImageLabel, TweenInfo.new(.5), { ImageTransparency = 0 }):Play() wait(2) TS:Create(ImageLabel, TweenInfo.new(.5), { ImageTransparency = 1 }):Play() wait(.5) ImageLabel:Destroy()
-
-game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(state) 
-    if state == Enum.TeleportState.InProgress then
-        reload_storm_function()
-    end
-end)
+--local TS = game:GetService('TweenService') local StormLoader = Instance.new('ScreenGui') local ImageLabel = Instance.new('ImageLabel') StormLoader.Name = 'StormLoader' StormLoader.Parent = game:GetService('CoreGui') StormLoader.ZIndexBehavior = Enum.ZIndexBehavior.Sibling StormLoader.Enabled = true ImageLabel.Parent = StormLoader ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255) ImageLabel.BackgroundTransparency = 1 ImageLabel.Position = UDim2.new(0.5, -100, 0.5, -100) ImageLabel.Size = UDim2.new(0, 200, 0, 200) ImageLabel.ZIndex = 1000877866 ImageLabel.Image = 'rbxassetid://12008234655' ImageLabel.ScaleType = Enum.ScaleType.Tile ImageLabel.ImageTransparency = 1 TS:Create(ImageLabel, TweenInfo.new(.5), { ImageTransparency = 0 }):Play() wait(2) TS:Create(ImageLabel, TweenInfo.new(.5), { ImageTransparency = 1 }):Play() wait(.5) ImageLabel:Destroy()
 
 --- Bit Library
 local M = {_TYPE='module', _NAME='bitop.funcs', _VERSION='1.0-0'}
